1296c95b84360d28962cf0c57cdd83bd3a3490c6
Kenta Utsunomiya <utsu.k.k@gmail.com>
2023-02-17 09:09:06 +0000

    Refactoring

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ FROM gradle:7.6.0-jdk17-alpine
 RUN mkdir /var/deploy
 COPY . /var/deploy
 WORKDIR /var/deploy
-RUN mkdir /var/storage
 ENTRYPOINT ["gradle", "bootRun"]
--- src/main/java/com/woven/movie_uploader/controllers/HealthController.java
+++ src/main/java/com/woven/movie_uploader/controllers/HealthController.java
@@ -1,11 +1,8 @@
 package com.woven.movie_uploader.controllers;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/${api.version}" )
